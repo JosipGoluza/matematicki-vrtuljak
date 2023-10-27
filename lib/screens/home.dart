f6b6f06bc9bdc9d83b6fd60cd99d714614b53ad8
JosipGoluza <josip.goluza12@gmail.com>
2023-10-27 15:26:50 +0200

    Reforamt code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -48,7 +48,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final audioPlayerProvider = Provider.of<AudioPlayerProvider>(
-      context, listen: false,);
+      context,
+      listen: false,
+    );
     AudioPlayer audioPlayer = audioPlayerProvider.audioPlayer;
     if (musicEnabled) {
       audioPlayer.play();
@@ -56,186 +58,185 @@ class _HomePageState extends State<HomePage> {
       audioPlayer.pause();
     }
 
-    final currentWidth = MediaQuery.of (context)
-    .size.width;
+    final currentWidth = MediaQuery.of(context).size.width;
     final currentHeight = MediaQuery.of(context).size.height;
     final signWidth = currentWidth * widthRatioHomeScreen;
     final signHeight = currentHeight * heightRatioHomeScreen;
 
     final isDesktop =
-    currentWidth >= isDesktopWidth || currentHeight >= isDesktopHeight;
+        currentWidth >= isDesktopWidth || currentHeight >= isDesktopHeight;
 
     return Scaffold(
-    body: Container(
-    decoration: const BoxDecoration(
-    image: DecorationImage(
-    image: AssetImage('assets/images/game_bg.png'),
-    fit: BoxFit.fill,
-    ),
-    ),
-    child: Stack(
-    children: [
-    Positioned(
-    top: 0,
-    right: 0,
-    child: Image(
-    fit: BoxFit.fill,
-    height: isDesktop ? 120 : 60,
-    image: const AssetImage(
-    'assets/images/sun.png',
-    ),
-    ),
-    ),
-    Padding(
-    padding: EdgeInsets.all(
-    isDesktop ? bigScreenPadding : smallScreenPadding,
-    ),
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    Container(
-    decoration: const BoxDecoration(
-    image: DecorationImage(
-    image: AssetImage("assets/images/pole.png"),
-    ),
-    ),
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: <Widget>[
-    const SizedBox(
-    height: signSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    startTimer(context, '/numbers', numberOfRounds,
-    numberOfAnswers);
-    // generateBeforeStart(context, '/numbers',
-    //     numberOfRounds, numberOfAnswers);
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_numbers.png'),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    const SizedBox(
-    height: signSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    startTimer(
-    context, '/different_sets', numberOfRounds);
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_sets.png'),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    const SizedBox(
-    height: signSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    startTimerOperators(
-    context,
-    '/operations_to_ten',
-    numberOfRounds,
-    numberOfAnswers,
-    operators,
-    chosenSymbol,
-    );
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_operations_10.png'),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    const SizedBox(
-    height: signSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    startTimerOperators(
-    context,
-    '/operations',
-    numberOfRounds,
-    numberOfAnswers,
-    operators,
-    chosenSymbol,
-    maxOperationNumber,
-    );
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_operations.png'),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    const SizedBox(
-    height: gamesAndSettingsSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    context.go('/settings', extra: _userPreferences);
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_settings.png'),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    const SizedBox(
-    height: signSeparator,
-    ),
-    InkWell(
-    onTap: () {
-    context.go('/about');
-    },
-    child: Image(
-    fit: BoxFit.fill,
-    image: const AssetImage(
-    'assets/images/hr/button_about.png',
-    ),
-    width: signWidth,
-    height: signHeight,
-    ),
-    ),
-    ],
-    ),
-    ),
-    Padding(
-    padding: EdgeInsets.only(bottom: currentHeight * 0.14),
-    child: Align(
-    alignment: Alignment.bottomRight,
-    child: Image(
-    fit: BoxFit.fill,
-    height: (currentHeight > 500)
-    ? currentHeight * 0.6
-        : currentHeight * 0.75,
-    image: const AssetImage(
-    'assets/images/hr/logo.png',
-    ),
-    ),
-    ),
-    )
-    ],
-    ),
-    ),
-    ],
-    ),
-    ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/game_bg.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Image(
+                fit: BoxFit.fill,
+                height: isDesktop ? 120 : 60,
+                image: const AssetImage(
+                  'assets/images/sun.png',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(
+                isDesktop ? bigScreenPadding : smallScreenPadding,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/pole.png"),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(
+                          height: signSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            startTimer(context, '/numbers', numberOfRounds,
+                                numberOfAnswers);
+                            // generateBeforeStart(context, '/numbers',
+                            //     numberOfRounds, numberOfAnswers);
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                                'assets/images/hr/button_numbers.png'),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: signSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            startTimer(
+                                context, '/different_sets', numberOfRounds);
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                                'assets/images/hr/button_sets.png'),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: signSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            startTimerOperators(
+                              context,
+                              '/operations_to_ten',
+                              numberOfRounds,
+                              numberOfAnswers,
+                              operators,
+                              chosenSymbol,
+                            );
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                                'assets/images/hr/button_operations_10.png'),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: signSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            startTimerOperators(
+                              context,
+                              '/operations',
+                              numberOfRounds,
+                              numberOfAnswers,
+                              operators,
+                              chosenSymbol,
+                              maxOperationNumber,
+                            );
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                                'assets/images/hr/button_operations.png'),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: gamesAndSettingsSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            context.go('/settings', extra: _userPreferences);
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                                'assets/images/hr/button_settings.png'),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: signSeparator,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            context.go('/about');
+                          },
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: const AssetImage(
+                              'assets/images/hr/button_about.png',
+                            ),
+                            width: signWidth,
+                            height: signHeight,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: currentHeight * 0.14),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Image(
+                        fit: BoxFit.fill,
+                        height: (currentHeight > 500)
+                            ? currentHeight * 0.6
+                            : currentHeight * 0.75,
+                        image: const AssetImage(
+                          'assets/images/hr/logo.png',
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
